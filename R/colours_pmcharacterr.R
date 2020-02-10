3dc25f68bd7c00f157517b517783e73837d88b3f
skgrange <s.k.grange@gmail.com>
2020-02-10 14:57:59 +0100

    :sparkles: better attribute handling for colours_pmcharacterr

--- R/colours_pmcharacterr.R
+++ R/colours_pmcharacterr.R
@@ -33,7 +33,7 @@ colours_pmcharacterr <- function(as_palette = TRUE) {
     x <- df %>% 
       pull(colour) %>% 
       "class<-"("palette") %>% 
-      setNames("colours_pmcharacterr")
+      `attr<-`("name", "pmcharacterr")
     
     return(x)
     
@@ -44,3 +44,17 @@ colours_pmcharacterr <- function(as_palette = TRUE) {
   }
   
 }
+
+
+# https://github.com/karthik/wesanderson/blob/master/R/colors.R
+#' print.palette <- function(x, ...) {
+#'   n <- length(x)
+#'   old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+#'   on.exit(par(old))
+#'   
+#'   image(1:n, 1, as.matrix(1:n), col = x,
+#'         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+#'   
+#'   rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+#'   text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+#' }
